9ce464bf7cf4d62f6d145337a2932303b406778f
Jeshurun Hembd <jjhembd@gmail.com>
2021-07-20 22:19:18 -0500

    Refactor zoom.js, moving tracking code to dragonfly.js

---
+++ src/dragonfly.js
@@ -0,0 +1,55 @@
+// TODO: Clean this up. Just use difference of lat/lon under ray?
+export function dragonflyStalk(camPos, zoomPos, zoomRay, ellipsoid) {
+  // See https://en.wikipedia.org/wiki/Dragonfly#Motion_camouflage
+  // Input camPos is the current geodetic position of the camera
+  // zoomPos is the ECEF position towards which we are zooming
+  // zoomRay is the camera ray (corresponding to a screen pixel) which pointed
+  //  toward zoomPos when the zoom action began
+  // The lon, lat in camPos will be adjusted to re-align zoomPos along zoomRay
+  //  (e.g., after a change in camera altitude)
+  const { abs, hypot, asin, cos, atan2 } = Math;
+
+  // Find the ray-sphere intersection in unrotated model space coordinates
+  const unrotatedCamPos = [0.0, 0.0, camPos[2] + ellipsoid.meanRadius()];
+  const target = new Float64Array(3);
+  const intersected = ellipsoid.shoot(target, unrotatedCamPos, zoomRay);
+  if (!intersected) return;
+
+  // Find the rotation about the y-axis required to bring zoomPos into the
+  // x = target[0] plane
+  const [zoomX, zoomY, zoomZ] = zoomPos;
+  const zoomR = hypot(zoomX, zoomZ);
+  if (zoomR < abs(target[0])) return;
+  const targetRotY = asin(target[0] / zoomR); // ?= atan2(target[0], target[2])
+  const rotY = atan2(zoomX, zoomZ) - targetRotY;
+
+  // After rotation around Y, zoomPos will be aligned with x = target[0].
+  // Now find the rotation around X to align with y = target[1]
+  const targetRotX = atan2(target[2], target[1]);
+  const zRotated = zoomR * cos(targetRotY);
+  const rotX = atan2(zRotated, zoomY) - targetRotX;
+
+  // Note signs: rotX ~ -latitude
+  const dLonLat = [rotY - camPos[0], -rotX - camPos[1]];
+  const limited = limitRotation(dLonLat);
+  camPos[0] += dLonLat[0];
+  camPos[1] += dLonLat[1];
+
+  return limited;
+}
+
+function limitRotation(dPos) {
+  const { abs, min, max, PI } = Math;
+  const maxRotation = 0.15;
+
+  // Check for longitude value crossing antimeridian
+  if (dPos[0] >  PI) dPos[0] -= 2.0 * PI;
+  if (dPos[0] < -PI) dPos[0] += 2.0 * PI;
+
+  if (abs(dPos[0]) < maxRotation) return false;
+
+  const tmp = min(max(-maxRotation, dPos[0]), maxRotation) / dPos[0];
+  dPos[0] *= tmp;
+  dPos[1] *= tmp;
+  return true;
+}
--- src/zoom.js
+++ src/zoom.js
@@ -1,10 +1,9 @@
-import * as vec3 from "gl-matrix/vec3";
 import { updateOscillator } from "./oscillator.js";
+import { dragonflyStalk } from "./dragonfly.js";
 
 export function initZoom(ellipsoid) {
   // Update camera altitude based on target set by mouse wheel events
   //  or two-finger pinch movements
-
   const w0 = 14.14; // Natural frequency of oscillator
   const minVelocity = 0.001;
 
@@ -13,28 +12,17 @@ export function initZoom(ellipsoid) {
   const dPos = new Float64Array(3);
 
   return function(position, velocity, cursor3d, deltaTime, track) {
-    // Input cursor3d is a pointer to an object
-    // Inputs position, velocity are pointers to 3-element arrays
-    // Input deltaTime is a primitive floating point value
+    const { zoomTarget, zoomPosition, zoomRay, stopZoom } = cursor3d;
 
-    let targetHeight = cursor3d.zoomTarget();
-
-    // Save old altitude
     const oldAltitude = position[2];
+    const targetHeight = zoomTarget();
 
     dPos[2] = position[2] - targetHeight;
     updateOscillator(position, velocity, dPos, w0, deltaTime, 2, 2);
 
-    let limited;
-    if (track) {
-      // Adjust rotation to keep zoom location fixed on screen
-      dPos.set(position);
-      dragonflyStalk(dPos, cursor3d.zoomRay, cursor3d.zoomPosition, ellipsoid);
-      // Restrict size of rotation in one time step
-      vec3.subtract(dPos, dPos, position);
-      limited = limitRotation(dPos);
-      vec3.add(position, position, dPos);
-    }
+    const limited = (track)
+      ? dragonflyStalk(position, zoomPosition, zoomRay, ellipsoid)
+      : false;
 
     // Scale rotational velocity by the ratio of the height change
     const heightScale = position[2] / oldAltitude;
@@ -48,68 +36,8 @@ export function initZoom(ellipsoid) {
     const extension = position[2] - targetHeight;
     const potentialE = 0.5 * (w0 * extension) ** 2;
     if (kineticE + potentialE < minEnergy * targetHeight) {
-      targetHeight = position[2]; // TODO: unnecessary?
       velocity[2] = 0.0;
-      cursor3d.stopZoom();
+      stopZoom();
     }
   };
-}
-
-function limitRotation(dPos) {
-  // Input dPos is a pointer to a 2-element array containing lon, lat changes
-  const { abs, min, max, PI } = Math;
-  const maxRotation = 0.15;
-
-  // Check for longitude value crossing antimeridian
-  if (dPos[0] >  PI) dPos[0] -= 2.0 * PI;
-  if (dPos[0] < -PI) dPos[0] += 2.0 * PI;
-
-  if (abs(dPos[0]) < maxRotation) return false;
-
-  const tmp = min(max(-maxRotation, dPos[0]), maxRotation) / dPos[0];
-  dPos[0] *= tmp;
-  dPos[1] *= tmp;
-  return true;
-}
-
-// Given a 3D scene coordinate over which a zoom action was initiated,
-// and a distance between the screen and the center of the 3D scene,
-// compute the rotations required to align the 3D coordinate along
-// the original screen ray.  See
-// https://en.wikipedia.org/wiki/Dragonfly#Motion_camouflage
-// TODO: Clean this up. Just use difference of lat/lon under ray?
-function dragonflyStalk(outRotation, ray, scenePos, ellipsoid) {
-  const { abs, hypot, asin, cos, atan2 } = Math;
-
-  const [sceneX, sceneY, sceneZ] = scenePos;
-
-  // Find the ray-sphere intersection in unrotated model space coordinates
-  const target = new Float64Array(3);
-  const unrotatedCamPos = [0.0, 0.0, outRotation[2] + hypot(...scenePos)];
-  const onEllipse = ellipsoid.shoot(target, unrotatedCamPos, ray);
-  if (!onEllipse) return; // No intersection!
-
-  // Find the rotation about the y-axis required to bring scene point into
-  // the  x = target[0]  plane
-  // First find distance of scene point from scene y-axis
-  const sceneR = hypot(sceneX, sceneZ);
-  // If too short, exit rather than tipping poles out of y-z plane
-  if (sceneR < abs(target[0])) return;
-  const targetRotY = asin(target[0] / sceneR); // Y-angle of target point
-  outRotation[0] =
-    atan2(sceneX, sceneZ) -     // Y-angle of scene vector
-    // asin( target[0] / sceneR ); // Y-angle of target point
-    targetRotY;
-
-  // We now know the x and y coordinates of the scene vector after rotation
-  // around the y-axis: (x = target[0], y = scenePos[1])
-  // Find the z-coordinate so we can compute the remaining required rotation
-  const zRotated = sceneR * cos(targetRotY);
-
-  // Find the rotation about the screen x-axis required to bring the scene
-  // point into the target y = target[1] plane
-  // Assumes 0 angle is aligned along Z, and angle > 0 is rotation toward -y !
-  outRotation[1] =
-    atan2(-1 * target[1], target[2]) - // X-angle of target point
-    atan2(-1 * sceneY, zRotated);      // X-angle of scene vector
 }
